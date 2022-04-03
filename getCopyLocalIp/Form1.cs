ad30bc336ec9b7bbb944f8a12d4afcd7cc60d086
Ramazan HABER <ramazanhaberr@gmail.com>
2022-04-03 05:15:06 +0300

    Add project files.

---
+++ getCopyLocalIp/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace getCopyLocalIp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string ipm = GetLocalIPAddress();
+            Clipboard.SetText(ipm);
+            MessageBox.Show("Local ip'niz kopyalandı...\n\nİP :   "+ipm);
+            this.Close();
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+            {
+                return "internete bağlı değil";
+            }
+
+            string localIP;
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("8.8.8.8", 65530);
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                localIP = endPoint.Address.ToString();
+                return localIP;
+            }
+        }
+
+    }
+}
